dffeb91ccd1967ee1d35f24ed6202725501bcd7b
anesanika <nikaanesiani06@gmail.com>
2025-04-15 05:38:11 -0700

    secure

--- cyber_backend/middleware/block_public_api.py
+++ cyber_backend/middleware/block_public_api.py
@@ -1,17 +1,20 @@
 import os
 from django.http import HttpResponseNotFound
+from django.conf import settings
 
 class BlockPublicAPI:
     def __init__(self, get_response):
         self.get_response = get_response
+        self.expected_token = os.getenv("FRONTEND_SECRET")
 
     def __call__(self, request):
-        api_paths = ['/api/', '/api/token/', '/api/token/refresh/', "/store/", "/store/products/", "/store/category/"]
-        if any(request.path.startswith(p) for p in api_paths):
-            frontend_token = request.headers.get("x-frontend-secret")
-            expected_token = os.getenv("FRONTEND_SECRET")
+        # admin page ან static/media ფაილები არ უნდა დაიბლოკოს
+        if request.path.startswith("/admin/") or request.path.startswith(settings.MEDIA_URL) or request.path.startswith(settings.STATIC_URL):
+            return self.get_response(request)
 
-            if frontend_token != expected_token:
-                return HttpResponseNotFound()
+        frontend_token = request.headers.get("x-frontend-secret")
+
+        if frontend_token != self.expected_token:
+            return HttpResponseNotFound()
 
         return self.get_response(request)
